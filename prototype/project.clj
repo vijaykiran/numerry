009d1f37a38295100efae31999c1ea870a8ed625
Adrien Haxaire <adrien@haxaire.org>
2016-03-03 21:42:48 +0100

    Updated info about project: decription and license

--- prototype/project.clj
+++ prototype/project.clj
@@ -1,8 +1,8 @@
 (defproject prototype "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Numerry's prototype"
+  :url "https://github.com/numerry/prototype"
+  :license {:name "Apache License 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot prototype.core
   :target-path "target/%s"
