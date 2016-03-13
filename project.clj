99952c02d6d223d51680c7b58177f8d213097453
Adrien Haxaire <adrien@haxaire.org>
2016-03-14 00:03:53 +0100

    Pfff should have tried it in repl sooner, stupid typo.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
-                 [environ "1.0.0"]]
+                 [environ "1.0.0"]
                  [clj-http "2.1.0"]
                  [clojurewerkz/neocons "3.1.0"]
                  [ring "1.4.0"]]
