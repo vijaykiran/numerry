82cd458da2f986f4cc058d4a084d9fcf6d5685f9
Adrien Haxaire <adrien@haxaire.org>
2016-03-14 00:02:13 +0100

    Fixing some config options for Heroku.

--- project.clj
+++ project.clj
@@ -9,12 +9,13 @@
                  [clj-http "2.1.0"]
                  [clojurewerkz/neocons "3.1.0"]
                  [ring "1.4.0"]]
-  :main ^:skip-aot prototype.core
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "prototype.jar"
   :target-path "target/%s"
-  :profiles {:production {:env {:production true}} :uberjar {:aot :all}})
+  :profiles {:production {:env {:production true}}})
 
 
+;;  :main ^:skip-aot prototype.core
+;;  :profiles {:uberjar {:aot :all}})
