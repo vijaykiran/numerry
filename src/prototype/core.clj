7065acd7bb64850f16dd8fa17c976650a35a7c06
Adrien Haxaire <adrien@haxaire.org>
2016-03-14 00:07:25 +0100

    Mismatching namespaces.

--- src/prototype/core.clj
+++ src/prototype/core.clj
@@ -1,26 +1,22 @@
 (ns prototype.core
-  (ns clojure-getting-started.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]])
-  (:gen-class)))
+            [environ.core :refer [env]]))
+
+;;  (:gen-class)))
 
 ;; (defn -main
 ;;   "I don't do a whole lot ... yet."
 ;;   [& args]
 ;;   (println "Hello, World!"))
 
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
-
-
-
-
+;; (defn handler [request]
+;;   {:status 200
+;;    :headers {"Content-Type" "text/html"}
+;;    :body "Hello World"})
 
 (defn splash []
   {:status 200
