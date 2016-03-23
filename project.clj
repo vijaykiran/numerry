e2c772e4bb6eaf854f2fd0faff037664c3e36a7c
Adrien Haxaire <adrien@haxaire.org>
2016-03-23 22:10:10 +0100

    Cleaned, preparing for ClojureScript.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prototype "0.1.0-SNAPSHOT"
   :description "Numerry's prototype"
-  :url "http://www.numerry.com"
+  :url "http://numerry.com"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -8,13 +8,12 @@
                  [compojure "1.4.0"]
                  [environ "1.0.0"]
                  [clj-http "2.1.0"]
-                 [clojurewerkz/neocons "3.1.0"]
                  [ring "1.4.0"]]
   :min-lein-version "2.0.0"
   :main ^:skip-aot prototype.core
   :plugins [[environ/environ.lein "0.3.1"]
             [lein-cljsbuild "1.1.3"]]
-  :hooks [environ.leiningen.hooks]     ;  leiningen.cljsbuild]
+  :hooks [environ.leiningen.hooks leiningen.cljsbuild]
 ;  :prep-tasks ["compile" ["cljsbuild" "once"]]
   :uberjar-name "prototype.jar"
   :profiles {:production {:env {:production true}} :uberjar {:aot :all}}
