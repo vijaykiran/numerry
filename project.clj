b96bea8eff3324d3b5a6e69ba140f52ef73a0c9a
Adrien Haxaire <adrien@haxaire.org>
2016-06-21 01:26:40 +0200

    Renamed prototype into numerry.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject prototype "0.1.0-SNAPSHOT"
-  :description "Numerry's prototype"
+  :description "Numerry's engine"
   :url "http://numerry.com"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.170"]]
   :min-lein-version "2.0.0"
-  :main ^:skip-aot prototype.core
+  :main ^:skip-aot numerry.core
   :plugins [[environ/environ.lein "0.3.1"] [lein-cljsbuild "1.1.3"]]
   :hooks [environ.leiningen.hooks leiningen.cljsbuild]
-  :uberjar-name "prototype.jar"
+  :uberjar-name "numerry.jar"
   :target-path "target/%s"
 
   :profiles {:production {:env {:production true}} :uberjar {:aot :all}}
