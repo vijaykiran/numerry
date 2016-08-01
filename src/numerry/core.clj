49a16fa8d9e53d8d6a2d278157f5343a5c2d54a3
Adrien Haxaire <adrien@haxaire.org>
2016-08-02 01:00:47 +0200

    Using jetty 9 in the core file.

--- src/numerry/core.clj
+++ src/numerry/core.clj
@@ -1,10 +1,10 @@
 (ns numerry.core
   (:gen-class)
-  (:require [ring.adapter.jetty :refer [run-jetty]]
+  (:require [ring.adapter.jetty9 :refer [run-jetty]]
             [compojure.handler :refer [site]]
             [numerry.routes :refer [main-routes]]))
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "8140"))]
     (run-jetty (site main-routes) {:port port :join? false})))
-
+      
