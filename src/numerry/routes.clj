2112288e72f97108f4459ad714114d85035df1d1
Adrien Haxaire <adrien@haxaire.org>
2016-08-02 01:01:21 +0200

    Get the raw data from a url.

--- src/numerry/routes.clj
+++ src/numerry/routes.clj
@@ -6,6 +6,7 @@
 (defroutes main-routes
   (GET "/" [] (home))
   (GET "/p/:id" [id] (plot id))
+  (GET "/p/:id/data" [id] (plot-data id))
   (GET "/plots" [] (plots))
   (route/resources "/")
   (route/not-found "<h1>Page not found</h1>"))
