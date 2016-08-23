a3296c5596b474da0c4fbf8c78703d98b401a400
Adrien Haxaire <adrien@haxaire.org>
2016-08-24 00:33:50 +0200

    Finished transition to Immutant.

--- src/numerry/core.clj
+++ src/numerry/core.clj
@@ -1,10 +1,26 @@
 (ns numerry.core
   (:gen-class)
   (:require [immutant.web :as web]
+            [immutant.web.async :as async]
             [compojure.handler :refer [site]]
-            [numerry.routes :refer [main-routes]]))
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [numerry.views :as views]
+            [numerry.websockets :refer [websockets]]))
+
+(def port
+  (Integer/parseInt (get (System/getenv) "PORT" "8140")))
+
+(defroutes main-routes
+  (GET "/" [] (views/home))
+  (GET "/p/:id" [id] (views/plot id))
+  (route/resources "/")
+  (route/not-found "<h1>Page not found</h1>"))
+
+(defn ws [request]
+  (async/as-channel request websockets))
 
 (defn -main []
-  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8140"))]
-    (web/run (site main-routes) {:port port})))
-      
+  (-> (web/run (site main-routes) :port port)
+      (assoc :path "/data")
+      (->> (web/run ws))))
--- src/numerry/websockets.clj
+++ src/numerry/websockets.clj
@@ -1,15 +1,14 @@
 (ns numerry.websockets
-  (:require [clojure.string :as str]
+  (:require [immutant.web.async :as async]
+            [clojure.string :as str]
             [cheshire.core :refer :all]
             [numerry.database :as db]))
 
-(defn on-text [ws pathname])
-  ;; (let [id (last (str/split (:uri (parse-string pathname true)) #"/"))
-  ;;       json (generate-string (:data (db/plot db/numerry id)))]
-  ;;   (jetty/send! ws json)))
+(defn on-message [ch pathname]
+  (let [id (last (str/split (:uri (parse-string pathname true)) #"/"))
+        json (generate-string (:data (db/plot db/numerry id)))]
+    (async/send! ch json)))
 
-(defn data-handler [req]
-  {:on-text (fn [ws text] (on-text ws text))})
-
-(def websockets {"/data" data-handler})
-
+(def websockets
+  {:on-message (fn [ch msg]
+                 (on-message ch msg))})
